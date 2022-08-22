ff9a3aa3b7e75307d55c6f00b1a8df8430f48025
mohammedSaadeh <muhammad.b.saadeh@gmail.com>
2022-08-22 20:22:44 +0300

    hornedbeast1

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class HornedBeast extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>{this.props.title} </h2>
+                <img src={this.props.imgageUrl} alt={this.props.title} title={this.props.title} />
+                <p></p>
+            </div>
+        )
+    }
+}
+export default HornedBeast;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HornedBeast from "./HornedBeast";
+
+class Main extends React.Component {
+    render(){
+        return(
+            <div>
+               <HornedBeast title="Rhino Family" imgageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" discription="Parent rhino with two babies"/>
+               <HornedBeast title="Unicorn Head" imgageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" discription="Someone wearing a very silly unicorn head mask"/> 
+            </div>
+        )
+    }
+}
+export default Main;
