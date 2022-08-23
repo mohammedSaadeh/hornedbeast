70765c5dd8ce0df85b3b993a7d040d78d881178e
mohammedSaadeh <muhammad.b.saadeh@gmail.com>
2022-08-24 02:47:00 +0300

    nf

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,14 +1,57 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 class HornedBeast extends React.Component{
+
+
+    constructor(props){ // to create a state
+        super(props);
+        this.state={
+            numberOfVotes : 0,
+            // otherVariable : 0 if i wanted to add another one
+        }
+   }
+
+
+   incrementNumOfVotes = () => {
+    this.setState({
+        numberOfVotes : this.state.numberOfVotes +1
+    })
+
+   }
+
+
     render(){
         return(
-            <div>
-                <h2>{this.props.title} </h2>
-                <img src={this.props.imgageUrl} alt={this.props.title} title={this.props.title} />
-                <p>{this.props.discription}</p>
-            </div>
-        )
+    //         <Card style={{ width: '18rem' }}>
+    //   <Card.Img variant="top" src={this.props.imgageUrl} onClick={this.incrementNumOfVotes}/>
+    //   <Card.Body>
+    //     <Card.Title>{this.props.title}</Card.Title>
+    //     <Card.Text>
+    //     🦄Number of votes: {this.state.numberOfVotes}
+    //     </Card.Text>
+    //     <Button variant="primary">Go somewhere</Button>
+    //   </Card.Body>
+    // </Card>
+      <Col>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    )
     }
 }
 export default HornedBeast;
